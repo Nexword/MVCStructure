d631bfd6b2ad6b0d99d851ccec3cfe1053a5518c
KAIGO-ACER\Kaigo <anatrim@yandex.ru>
2017-04-09 03:26:34 +0300

    add controller, view and createdatabase (code-first)

---
+++ EFDemoProject/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFDemoProject.Models.EF;
+
+namespace EFDemoProject.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private BugTrackerContext db = new BugTrackerContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+    }
+}
