876933ad9fb61dba1f808965489bb8ef36675b65
Mikhail Dudin <dudin-m@live.ru>
2017-04-10 22:17:03 +0300

    Issue/Index

--- EFDemoProject/Controllers/IssueController.cs
+++ EFDemoProject/Controllers/IssueController.cs
@@ -1,14 +1,27 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using EFDemoProject.Models.EF;
 using EFDemoProject.ViewModels;
 
 namespace EFDemoProject.Controllers
 {
     public class IssueController : Controller
     {
+		private readonly BugTrackerContext context = new BugTrackerContext();
+		
         // GET: Issue
         public ActionResult Index()
         {
-            return View();
+	        var issues = context.Issues.AsEnumerable();
+			var model = issues.Select(x => new IssueViewModel
+	        {
+				Id = x.Id,
+				Title = x.Title,
+				Status = x.Status.ToString(),
+				Price = x.Price
+	        });
+
+            return View(model);
         }
 
 	    public ActionResult Get(int id)
---
+++ EFDemoProject/ViewModels/IssueViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EFDemoProject.ViewModels
+{
+	public class IssueViewModel
+	{
+		public int Id { get; set; }
+
+		public string Title { get; set; }
+
+		public decimal Price { get; set; }
+
+		public string Status { get; set; }
+	}
+}
