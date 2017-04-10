b15f9311c14ca79b9e6f074792740af253326651
Mikhail Dudin <dudin-m@live.ru>
2017-04-10 22:07:50 +0300

    added empty IssueController and create-vm

---
+++ EFDemoProject/Controllers/IssueController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using EFDemoProject.ViewModels;
+
+namespace EFDemoProject.Controllers
+{
+    public class IssueController : Controller
+    {
+        // GET: Issue
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+	    public ActionResult Get(int id)
+	    {
+		    return View();
+	    }
+
+	    public ActionResult Create()
+	    {
+		    return View();
+	    }
+
+		[HttpPost]
+	    public ActionResult Create(IssueCreateModel model)
+	    {
+		    return View();
+	    }
+    }
+}
