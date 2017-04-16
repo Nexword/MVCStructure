ad02c6cafa79ab51afa62d1d1e2d992bb39e18bf
KAIGO-ACER\Kaigo <anatrim@yandex.ru>
2017-04-17 01:23:14 +0300

    add domain interfaces

---
+++ EFDemoProject.Domain.Interfaces/IIssueRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using EFDemoProject.Domain.Core.Entities;
+
+namespace EFDemoProject.Domain.Interfaces
+{
+    public interface IIssueRepository
+    {
+        IEnumerable<Issue> GetIssueList();
+        Issue GetIssue(int id);
+        void Create(Issue item);
+        void Update(Issue item);
+        void Delete(int id);
+        void Save();
+    }
+}
